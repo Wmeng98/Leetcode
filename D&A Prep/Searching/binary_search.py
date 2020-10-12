0e6bc4b1c034146f81a7f493e7285b3b0825bb8e
Wilson Meng <w5meng@uwaterloo.ca>
2020-10-12 02:34:37 -0400

    Binary search impl

---
+++ D&A Prep/Searching/binary_search.py
@@ -0,0 +1,41 @@
+def iterativeBS(arr, val):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        # mid = ((high - low + 1) // 2) + low
+        mid = (low + high) // 2
+        if arr[mid] == val:
+            return mid
+        if arr[mid] < val:
+            # constrain search to right half
+            low = mid + 1
+        else:
+            # constrain search to left half
+            high = mid - 1
+    return -1
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+arr2 = [1,2,3,4,5,6,7,8,9]
+print('iterative binary search', iterativeBS(arr, 3))
+print('iterative binary search', iterativeBS(arr2, 8))
+
+
+def recursiveBS(arr, val, low, high):
+    if low > high:
+        return -1
+
+    mid = (low + high) // 2
+    if arr[mid] == val:
+        return mid
+    elif arr[mid] < val:
+        # recurse on right half
+        return recursiveBS(arr, val, mid + 1, high)
+    else:
+        # recurse on left half
+        return recursiveBS(arr, val, low, mid - 1)
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+arr2 = [1,2,3,4,5,6,7,8,9]
+print('recursive binary search', recursiveBS(arr, 3, 0, len(arr) - 1))
+print('recursive binary search', recursiveBS(arr2, 8, 0, len(arr2) - 1))   
